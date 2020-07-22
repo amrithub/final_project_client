d89134121bbd374be5df5fc35c789237d36adedf
amrit <amritsk1@yahoo.com>
2020-07-22 19:05:33 +1000

    testing client deployment

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-
 // Create an axios instance
 export default axios.create({
-  baseURL: 'http://localhost:3009',
-  timeout: 5000
+  baseURL: 'https://gentle-tundra-64079.herokuapp.com/',
+  timeout: 10000,
+  withCredentials: true
 })
