b3e20843a759fb18548e7a6d73940d1dac37e57d
amrit <amritsk1@yahoo.com>
2020-07-25 15:46:20 +1000

    edit function integrated successfully

--- src/components/EditDishPost.js
+++ src/components/EditDishPost.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {withRouter} from 'react-router-dom'
 import {useGlobalState} from '../config/store'
-import {getPostFromId} from '../services/dishPostServices'
+import {getPostFromId,updateDishPost} from '../services/dishPostServices'
 
 const EditDishPost = ({history, match}) => {
     const {store, dispatch} = useGlobalState()
@@ -36,19 +36,29 @@ const EditDishPost = ({history, match}) => {
         event.preventDefault()
         const updatedPost = {
             _id: post._id,
+            username: post.username,
             title: formState.title,
             category: formState.category || "general",
             modified_date: new Date(),
             content: formState.content
         }
-        const otherPosts = dishPosts.filter((post) => post._id !== updatedPost._id)
-        dispatch({
-            type: "setDishPosts",
-            data: [...otherPosts, updatedPost]
+        updateDishPost(updatedPost).then(() => {
+            const otherPosts = dishPosts.filter((post) => post._id !== updatedPost._id)
+            dispatch({
+                type: "setDishPosts",
+                data: [updatedPost, ...otherPosts]
+            })
+            history.push(`/posts/${post._id}`)
+        }).catch((error) => {
+            const status = error.response ? error.response.status : 500
+            console.log("caught error on edit", error)
+            if(status === 403)
+                setErrorMessage("Oops! It appears we lost your login session. Make sure 3rd party cookies are not blocked by your browser settings.")
+            else
+                setErrorMessage("Well, this is embarrassing... There was a problem on the server.")
         })
-        history.push(`/posts/${post._id}`)
     }
-    // Set initial form values to what is in the current post
+    
     const initialFormState = {
         title: "",
         category: "",
@@ -56,7 +66,7 @@ const EditDishPost = ({history, match}) => {
     } 
 
     const [formState,setFormState] = useState(initialFormState)
-
+    const [errorMessage, setErrorMessage] = useState(null)
     useEffect(() => {
         // Set the formState to the fields in the post after mount and when post changes
         post && setFormState({
